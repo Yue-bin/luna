8dc7322e3d8f4c4da5129f57a890be58ac6ebc5e
Yue-bin <3103174284@qq.com>
2026-06-17 19:57:25 +0800

    feat(app): 添加随机数池管理和API接口
    - 引入pools模块和配置管理，支持多随机数池
    - 新增/list-pools API用于列出可用的随机数池
    - 修改/number API支持通过pool参数指定使用的随机数池
    - 默认使用配置中的第一个池作为默认池

--- app.lua
+++ app.lua
@@ -2,19 +2,39 @@ local lapis = require("lapis")
 local app = lapis.Application()
 local bint = require("bint")(256)
 
-local rng = require("rng")
+local pools = require("pools")
+--- @cast pools RngPoolList
+local config = require("rng_config")
+
+-- 默认使用第一个池
+local default_pool = config[1].name
+
+app:get("/random/list-pools", function(self)
+  local pools_list = {}
+  for _, pool_config in ipairs(config) do
+    table.insert(pools_list, pool_config.name)
+  end
+  return {
+    json = {
+      pools = pools_list,
+    },
+  }
+end)
 
 app:get("/random/number", function(self)
   local lower = bint.fromstring(self.params.lower) or bint.zero()
   local upper = bint.fromstring(self.params.upper) or bint.frominteger(100)
+  local pool = self.params.pool or default_pool
   --- @cast upper bint
   --- gosh i need `shouldfrom` like same in go
-  local random_number = rng.random_number(lower, upper)
+  local random_number = pools[pool]:random_number(lower, upper)
   return {
     json = {
       number = tostring(random_number),
     },
   }
 end)
+
+
 
 return app
